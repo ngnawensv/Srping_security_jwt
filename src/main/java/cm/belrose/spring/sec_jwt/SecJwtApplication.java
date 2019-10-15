f486d3afefb38eed8e24b7fe9377c08b2996c9c4
ngnawen samuel vermon <ngnawens@gmail.com>
2019-10-15 02:01:42 +0100

    First commit

---
+++ src/main/java/cm/belrose/spring/sec_jwt/SecJwtApplication.java
@@ -0,0 +1,13 @@
+package cm.belrose.spring.sec_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecJwtApplication.class, args);
+	}
+
+}
